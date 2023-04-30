eec7807453e05bb00cb655e36675f36a83dd9de1
Franco Felicioni <fran.felicioni@gmail.com>
2023-04-30 09:01:25 +0200

    Creating a counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import { ref } from 'vue';
+    const counter = ref(0)
+    const increment = () => { counter.value ++ }
+
+</script>
+
+<template>
+
+    <div>
+        <button @click="increment">Counter: {{ counter }}</button>
+    </div>
+
+</template>
+
+<style></style>
