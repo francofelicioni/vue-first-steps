d590ffc1c0aab329ad5c8bf5a8be727191312262
Franco Felicioni <fran.felicioni@gmail.com>
2023-04-30 18:57:51 +0200

    Using emit

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,33 @@
 
 import { ref } from 'vue';
 import BlogPost from './components/BlogPost.vue';
-import Counter from './components/Counter.vue';
+
 
 const posts = ref([
-  {id:1, title: 'Campeonato', text:'Copa merecimiento 2009'},
-  {id:2, title: 'Campeonato', text:'Copa lloron 2013'},
-  {id:3, title: 'Campeonato', text:'Copa equipo diminuto 2014'},
+  {id:1, title: 'Campeonato 1', text:'Copa merecimiento 2009'},
+  {id:2, title: 'Campeonato 2', text:'Copa lloron 2013'},
+  {id:3, title: 'Campeonato 3', text:'Copa equipo diminuto 2014'},
 ])
 
+const favorite = ref('')
+
+const changeFavorite = (title) => {
+  favorite.value = title;
+}
 
 </script>
 
 
 <template>
-  <div  class="flex flex-col justify-center items-center p-10">
-    <h1 class="mb-10 text-4xl m-3 text-center w-fit"> YEARS WITHOUT HURACAN´T CAMPEON</h1>
-    <Counter />
-  </div>
-
   <div>
-    <h2 class="text-center text-2xl underline">Palmares de buri en su historia:</h2>
+    <h1>APP</h1>
+    <h2 class="text-center text-2xl underline">Mi loved campeonatos: {{ favorite }}</h2>
     <BlogPost 
     v-for= 'post in posts'
     :key='post.id'
     :title="post.title"
     :text="post.text"
+    @changeFavorite="changeFavorite"
     ></BlogPost>
   </div>
 </template>
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,26 +1,35 @@
 <script setup>
-    // defineProps(['title', 'id', 'text', 'fontColor' ])
+// defineProps(['title', 'id', 'text', 'fontColor' ])
 
-    defineProps({
-        title: String,
-        id: Number,
-        text: {
-            type: String,
-            default: 'No description'
-        },
-        fontColor: String
-    })
+defineProps({
+    title: {
+        type: String,
+        default: 'No title'
+    },
+    id: Number,
+    text: {
+        type: String,
+        default: 'No description'
+    },
+    fontColor: String
+})
 </script>
 
 
 <template>
     <div class="p-3 w-2/4 content-center m-auto">
         <div class="flex flex-col align-center justify-center bg-white rounded">
-            <h5 class="text-black text-center text-xl">{{ id }} - {{title}}</h5>
-            <p :class="`text-${fontColor}-500`" class="text-black text-center text-xl">{{text}}</p>
+            <h5 class="text-black text-center text-xl">
+                {{ id }} - {{ title }}
+            </h5>
+            <p :class="`text-${fontColor}-500`" class="text-black text-center text-xl">
+                {{ text }}
+            </p>
+            <button @click="$emit('changeFavorite', title)" class="bg-blue-500 font-white font-bold">
+                Change favorite
+            </button>
         </div>
     </div>
 </template>
 
-<style>
-</style>
+<style></style>
